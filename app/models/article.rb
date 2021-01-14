18bacf836b6196ae88cad2724a93ca716e6c6fa3
Gzim <gzimasanni@gmail.com>
2021-01-14 16:30:17 +0100

    Add create articles basic page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :article_categories
   has_many :categories, through: :article_categories
+
+  validates :title, presence: true, length: {minimum: 6, maximum: 80}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
