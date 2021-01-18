87eb8176ed5e0325d25dd48943371dfa95b845d3
Gzim <gzimasanni@gmail.com>
2021-01-18 16:54:46 +0100

    Add users controller and basic signup page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles, only: [:show, :new, :create]
   resources :categories, only: [:show, :index] 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
 end
