e255679049f30a516203890c0031653ddca20879
Gzim <gzimasanni@gmail.com>
2021-01-18 16:27:04 +0100

    Add navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 4, maximum: 24}, uniqueness: { case_sensitive: false }
   has_many :articles, foreign_key: :author_id
 end
